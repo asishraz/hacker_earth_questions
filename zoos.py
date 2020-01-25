0252c13c1f9c9dc5ef67f4c257db86c20ac2d4b8
asishraz <iamasish3001@gmail.com>
2020-01-25 23:30:54 +0530

    added new file

---
+++ zoos.py
@@ -0,0 +1,13 @@
+word = input().lower()
+
+list_of_chars = list(word)
+
+def to_check_for_z_o(string):
+    if 'z' and 'o' in string:
+        #do the process
+        
+    else:
+        print("only applicable for 'z' and 'o' ")
+        
+        
+myfunc = to_check_for_z_o(word)
